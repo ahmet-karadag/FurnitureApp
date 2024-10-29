338de9229f8a07e7fca9e37a40c6cfa45d356a04
Ahmet Karadağ <akkaradaga@gmail.com>
2024-10-29 13:36:33 +0300

    Initial Commit

---
+++ Furniture/FurnitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureApp.swift
+//  Furniture
+//
+//  Created by ahmet karadağ on 29.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
